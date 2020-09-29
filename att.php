0cd1ee06a08d7f0e2c7ac6e2d18753342c26f85b
mansish17 <48938334+mansish17@users.noreply.github.com>
2020-09-29 13:14:56 +0530

    Add files via upload

---
+++ att.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body><form>
+        <div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+<div style="padding-left:16px">
+  <table rowspan="20px" padding="13px"  border="1px"><div>
+      <tr>
+          <th>
+              ROLL NO
+          </th>
+          <th>
+              PRESENT
+          </th>
+          <th>
+              ABSENT
+          </th>
+      </tr>
+      <tr>
+      <td>1</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="1">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="1">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>2</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="2">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="2">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>3</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="3">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="3">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>4</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="4">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="4">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>5</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="5">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="5">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>6</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="6">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="6">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>7</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="7">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="7">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>8</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="8">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="8">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>9</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="9">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="9">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>10</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="10">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="10">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>11</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="11">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="11">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>12</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="12">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="12">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>13</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="13">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="13">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>14</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="14">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="14">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>15</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="15">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="15">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>16</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="16">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="16">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>17</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="17">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="17">
+  <span class="checkmark"></span>
+</label></td>
+      </tr><tr>
+      <td>18</td>
+          <td>
+              <label class="container">
+  <input type="radio" name="18">
+  <span class="checkmark"></span>
+</label>
+          </td>
+          <td><label class="content">
+  <input type="radio" name="18">
+  <span class="checkmark"></span>
+</label></td>
+      </tr></div>
+  </table><br><center><div id ="button">
+<input type="submit" value="submit" id="sub" ><hr noshade width="1px">
+<input type="reset" id="res"></div></center>
+</form>
+
+    </body>
+</html>
---
+++ new.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>EDUVATIONAL</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+    <!-- Write your code below -->
+    <div class="header">
+      <div class="header-logo">Eduvational</div>
+      <div class="header-list">
+        <ul>
+          <li>Attendance</li>
+          <li>Lessons</li>
+          <li>Queries</li>
+          
+        </ul>
+      </div>
+    </div>
+      
+   
+  </body>
+</html>
